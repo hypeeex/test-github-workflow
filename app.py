a1a374f105c2821f20412722bf3ae99e71756047
virtuoso <hypeeex@gmail.com>
2023-05-08 01:27:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+"""
+ A simple endpoint that returns a greeting message.
+"""
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    A simple endpoint that returns a greeting message.
+    """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
